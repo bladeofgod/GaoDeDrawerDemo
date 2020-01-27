8a205e4930bede315eff179479ce349b6e6795d3
bladeofgod <450103187@qq.com>
2020-01-27 14:11:38 +0800

    anthor one

--- app/src/main/java/com/bedrock/gaodedrawerdemo/MainActivity.java
+++ app/src/main/java/com/bedrock/gaodedrawerdemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.bedrock.gaodedrawerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.lxj.xpopup.XPopup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test();
+
+    }
+
+    private void test(){
+        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void showBottomDrawer(){
+        new XPopup.Builder(this)
+                .offsetY(-200)
+                .asCustom(new BottomDrawer(this))
+                .show();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
